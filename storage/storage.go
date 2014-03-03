54c6eeb33c800368a442c5a23e793fb1370ccd65
jake <bigjake2s@gmail.com>
2014-03-03 16:52:17 -0500

    Added wrapper funcs around the storage backend

--- storage/storage.go
+++ storage/storage.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"github.com/300brand/spider/config"
+	"github.com/300brand/spider/page"
 	"github.com/300brand/spider/storage/backend"
 )
 
@@ -13,3 +15,9 @@ func New(b backend.Backend) (s *Storage) {
 		Backend: b,
 	}
 }
+
+func (s *Storage) Close() (err error)                           { return s.Backend.Close() }
+func (s *Storage) GetPage(url string, p *page.Page) (err error) { return s.Backend.GetPage(url, p) }
+func (s *Storage) SavePage(p *page.Page) (err error)            { return s.Backend.SavePage(p) }
+func (s *Storage) GetConfig(c *config.Config) (err error)       { return s.Backend.GetConfig(c) }
+func (s *Storage) SaveConfig(c *config.Config) (err error)      { return s.Backend.SaveConfig(c) }

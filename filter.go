74ced3f6981a6b7680daeef38e576cea620c999e
jake <bigjake2s@gmail.com>
2014-08-06 10:36:00 -0400

    Commented out all filters except TMC Net

--- filter.go
+++ filter.go
@@ -25,38 +25,38 @@ var filters = map[string]Filter{
 		},
 		Reject: []*regexp.Regexp{},
 	},
-	"ccweek.com": Filter{
-		Ident:       "ccweek",
-		Start:       "http://ccweek.com/",
-		CSSSelector: "a[href]",
-		Restart:     30 * time.Minute,
-		Accept: []*regexp.Regexp{
-			regexp.MustCompile(`^/article-\d{4,}`),
-		},
-		Reject: []*regexp.Regexp{},
-	},
-	"www.pipelinepub.com": Filter{
-		Ident:       "pipelinepub",
-		Start:       "http://www.pipelinepub.com/",
-		CSSSelector: ".leftsidelinks a[href]",
-		Restart:     30 * time.Minute,
-		Accept:      []*regexp.Regexp{},
-		Reject:      []*regexp.Regexp{},
-	},
-	"www.healthmgttech.com": Filter{
-		Ident:       "healthmgttech",
-		Start:       "http://www.healthmgttech.com/",
-		CSSSelector: "a[href]",
-		Restart:     30 * time.Minute,
-		Accept: []*regexp.Regexp{
-			regexp.MustCompile(`^/articles/\d{6}/`),
-			regexp.MustCompile(`/news/.`),
-			regexp.MustCompile(`/blogs/.`),
-			regexp.MustCompile(`/online-only/.`),
-		},
-		Reject: []*regexp.Regexp{
-			regexp.MustCompile(`/news/all-news.php`),
-			regexp.MustCompile(`/articles/\d{6}/toc.php`),
-		},
-	},
+	// "ccweek.com": Filter{
+	// 	Ident:       "ccweek",
+	// 	Start:       "http://ccweek.com/",
+	// 	CSSSelector: "a[href]",
+	// 	Restart:     30 * time.Minute,
+	// 	Accept: []*regexp.Regexp{
+	// 		regexp.MustCompile(`^/article-\d{4,}`),
+	// 	},
+	// 	Reject: []*regexp.Regexp{},
+	// },
+	// "www.pipelinepub.com": Filter{
+	// 	Ident:       "pipelinepub",
+	// 	Start:       "http://www.pipelinepub.com/",
+	// 	CSSSelector: ".leftsidemenu a[href]",
+	// 	Restart:     30 * time.Minute,
+	// 	Accept:      []*regexp.Regexp{},
+	// 	Reject:      []*regexp.Regexp{},
+	// },
+	// "www.healthmgttech.com": Filter{
+	// 	Ident:       "healthmgttech",
+	// 	Start:       "http://www.healthmgttech.com/",
+	// 	CSSSelector: "a[href]",
+	// 	Restart:     30 * time.Minute,
+	// 	Accept: []*regexp.Regexp{
+	// 		regexp.MustCompile(`^/articles/\d{6}/`),
+	// 		regexp.MustCompile(`/news/.`),
+	// 		regexp.MustCompile(`/blogs/.`),
+	// 		regexp.MustCompile(`/online-only/.`),
+	// 	},
+	// 	Reject: []*regexp.Regexp{
+	// 		regexp.MustCompile(`/news/all-news.php`),
+	// 		regexp.MustCompile(`/articles/\d{6}/toc.php`),
+	// 	},
+	// },
 }
